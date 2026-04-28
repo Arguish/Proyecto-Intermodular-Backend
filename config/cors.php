7ff0b9192cddf46c919daf089bfdd87c79c3d26d
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2026-04-28 21:33:22 +0100

    WIP allow all

--- config/cors.php
+++ config/cors.php
@@ -1,16 +1,11 @@
 <?php
 
-$originsFromEnv = array_values(array_filter(array_map(
-    'trim',
-    explode(',', (string) env('CORS_ALLOWED_ORIGINS', env('FRONTEND_URL', '*')))
-)));
-
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => $originsFromEnv,
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
